081bdf62c18e889cb536e4fa9545c6b759b4854c
Isaac Robinson <awesomeisaac2000@gmail.com>
2022-03-03 19:43:13 -0700

    Make QT toolkit swappable...

--- CursorCreate/gui/QtKit.py
+++ CursorCreate/gui/QtKit.py
@@ -1,14 +1,20 @@
-try:
-    from PySide6 import *
-except ImportError:
-    try:
-        from PySide2 import *
-    except ImportError:
-        try:
-            from PyOt6 import *
-        except ImportError:
-            try:
-                from PyQt5 import *
-            except ImportError:
-                raise ImportError("Unable to find a Qt toolkit for the UI!")
+from typing import Any, List
 
+_QT_Implementations = ["PySide6", "PySide2", "PyQt6", "PyQt5"]
+_QT_IMPL = None
+
+for impl_name in _QT_Implementations:
+    try:
+        _QT_IMPL = __import__(impl_name, globals(), locals(), [], 0)
+        continue
+    except ImportError:
+        pass
+
+if(_QT_IMPL is None):
+    raise ImportError("Unable to find a Qt toolkit for the UI!")
+
+def __getattr__(attr: str) -> Any:
+    return getattr(_QT_IMPL, attr)
+
+def __dir__() -> List[str]:
+    return dir(_QT_IMPL)
